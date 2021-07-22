8959c80f10b6f8fb01b394abb9389a875b2763b6
av8ta <av8ta@protonmail.com>
2021-07-22 12:10:33 +0800

    remove ts-node and simply watch with tsc

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,20 @@
+import pull from 'pull-stream'
+
 const plugin = {
   name: 'sharp',
   version: require('../package.json').version,
-  manifest: {},
+  manifest: {
+    get: 'source'
+  },
   init(rpc: any) {
     console.log('sharp plugin loaded')
+
+    // prettier-ignore
+    pull(
+      pull.values([1, 2, 3,4,5]),
+      pull.collect(console.log)
+
+    )
   }
 }
 
